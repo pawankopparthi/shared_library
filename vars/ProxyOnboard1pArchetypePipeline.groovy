502be9975735bfa4be16e398a0b7a42c679f5097
pawankopparthi <99384689+pawankopparthi@users.noreply.github.com>
2023-09-24 10:55:48 +0530

    Update ProxyOnboard1pArchetypePipeline.groovy

--- vars/ProxyOnboard1pArchetypePipeline.groovy
+++ vars/ProxyOnboard1pArchetypePipeline.groovy
@@ -63,7 +63,7 @@ def call() {
         //def nodeHome = tool name: DefaultConfigService.instance.tools.nodejs, type: 'Node16'
 
         //Not required as maven is fixed using runCommand in below function
-        def mvnHome = tool name: DefaultConfigService.instance.tools.maven, type: 'maven'
+       // def mvnHome = tool name: DefaultConfigService.instance.tools.maven, type: 'maven'
 
         echo "Maven Home is ${mvnHome}"
         def mvnExecutable = "${mvnHome}/usr/share/maven"
