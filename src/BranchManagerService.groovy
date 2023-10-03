2452098afd63e674aef5605d7a41f246786a5184
pawankopparthi <99384689+pawankopparthi@users.noreply.github.com>
2023-10-03 18:06:55 +0530

    Update BranchManagerService.groovy

--- src/BranchManagerService.groovy
+++ src/BranchManagerService.groovy
@@ -173,8 +173,8 @@ void createRelease(String api ) {
     jgitflow:release-start -X
 
   '''*/
-  runGitflowCommands("-DallowSnapshots=true -DautoVersionSubmodules=true -DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true -s /usr/share/maven/conf/settings.xml" , "jgitflow:release-start -X" )
-  //mvn "-DallowSnapshots=true -DautoVersionSubmodules=true " + "-DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true", "jgitflow:release-start -X" 
+  //runGitflowCommands("-DallowSnapshots=true -DautoVersionSubmodules=true -DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true -s /usr/share/maven/conf/settings.xml" , "jgitflow:release-start -X" )
+  mvn "-DallowSnapshots=true -DautoVersionSubmodules=true  -DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true", "jgitflow:release-start -X" 
 
 }
 
