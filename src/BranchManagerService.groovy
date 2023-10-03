eae3d8ee4a57d17050956ae773671dfdefd99353
pawankopparthi <99384689+pawankopparthi@users.noreply.github.com>
2023-10-03 11:54:35 +0530

    Update BranchManagerService.groovy

--- src/BranchManagerService.groovy
+++ src/BranchManagerService.groovy
@@ -167,12 +167,11 @@ void createRelease(String api ) {
     log.fatal "A current Release exist. Finish the Release before starting a new one. " +
             "Stopping Job "
   }
- /* sh '''
-  "-DallowSnapshots=true -DautoVersionSubmodules=true " +
-          "-DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true" -s /usr/share/maven/settings.xml, "jgitflow:release-start -X" 
-  '''*/
-  runGitflowCommands("-DallowSnapshots=true -DautoVersionSubmodules=true " +
-          "-DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true"  -s /usr/share/maven/settings.xml, "jgitflow:release-start -X" )
+  sh '''
+  "-DallowSnapshots=true -DautoVersionSubmodules=true " + "-DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true" -s /usr/share/maven/settings.xml, "jgitflow:release-start -X" 
+  '''
+  //runGitflowCommands("-DallowSnapshots=true -DautoVersionSubmodules=true " +
+    //      "-DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true"  -s /usr/share/maven/settings.xml, "jgitflow:release-start -X" )
 
 }
 
