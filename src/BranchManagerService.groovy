0fe86abb69882764c82d392feee04eeed6a14dd2
pawankopparthi <99384689+pawankopparthi@users.noreply.github.com>
2023-10-04 15:08:33 +0530

    Update BranchManagerService.groovy

--- src/BranchManagerService.groovy
+++ src/BranchManagerService.groovy
@@ -171,6 +171,15 @@ void createRelease(String api ) {
   mvn -DallowSnapshots=true -DautoVersionSubmodules=true -DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true jgitflow:release-start -X -s /usr/share/maven/conf/settings.xml
 
   '''*/
+  sh """
+mvn -B -P gitflow -DpushFeatures=true \\
+  -DallowSnapshots=true -DautoVersionSubmodules=true \\
+  -DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true \\
+  -DscmUsername=pawankopparthi -DscmPassword=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 \\
+  -s /usr/share/maven/conf/settings.xml \\
+  jgitflow:release-start -X
+"""
+
  sh  "mvn -s /usr/share/maven/conf/settings.xml -DscmUsername=pawankopparthi -DscmPassword=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 -DallowSnapshots=true -DautoVersionSubmodules=true -DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true jgitflow:release-start -X"
 
   // runGitflowCommands("-DallowSnapshots=true -DautoVersionSubmodules=true -DreleaseBranchVersionSuffix=RC1 -DupdateDependencies=true ", "jgitflow:release-start -X" ) -s /usr/share/maven/conf/settings.xml
